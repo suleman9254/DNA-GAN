1dce75282dd8028a9082ca769b3ab3817d86143e
Suleman <sul103@csiro.au>
2024-02-08 10:16:33 +1100

    added training loop

--- modules/model.py
+++ modules/model.py
@@ -1,9 +1,10 @@
 import torch
 from torch.nn import Module, GRU, Linear, CrossEntropyLoss
 import torch.nn.functional as F
+from torch.optim import Adam
 
-from torch.utils.data import DataLoader
 from tqdm import tqdm
+import wandb
 
 class Generator(Module):
     def __init__(self, hidden_size, num_layers):
@@ -41,12 +42,59 @@ class Discriminator(Module):
         y = self.fc(y)
         return F.sigmoid(y)
 
-def train(model, batch_size, seq_len, device):
-
+def generate_then_discriminate(model, batch_size, seq_len, criterion, target, device):
     z = torch.randn((batch_size, seq_len), device=device)
     syn = model.generator(z)
     
-        
+    label = torch.full((batch_size,), target, device=device)
+    d_pred = model.discriminator(syn)
+    err = criterion(d_pred, label)
+    return err
+
+def train(model, train_data, batch_size, seq_len, g_solver, d_solver, criterion, device):
+    real_label, fake_label = 1, 0
+
+    # Generator Training
+    model.generator.train(True)
+    model.discriminator.eval()
+    
+    for g_step in range(2):
+        g_solver.zero_grad()
+        err_G = generate_then_discriminate(model, 
+                                           batch_size, 
+                                           seq_len, 
+                                           criterion, 
+                                           real_label, 
+                                           device)
+        err_G.backward()
+        g_solver.step()
+
+    # Discriminator Training
+    model.generator.eval()
+    model.discriminator.train(True)
+
+    d_solver.zero_grad()
+    err_fake = generate_then_discriminate(model, 
+                                          batch_size, 
+                                          seq_len, 
+                                          criterion, 
+                                          fake_label,
+                                          device)
+    
+    real = batch_generator(train_data, batch_size)
+    real = real.to(device)
+    
+    d_pred = model.discriminator(real)
+    label = torch.full((batch_size,), real_label, device=device)
+    err_real = criterion(d_pred, label)
+
+    err_D = torch.mean(err_real.item(), err_fake.item())
+    if err_D > 0.15:
+        err_fake.backward()
+        err_real.backward()
+        d_solver.step()
+
+    return model, err_G, err_D
 
 class DNA_GAN(Module):
     def __init__(self, hidden_size, num_layers, seq_len):
@@ -57,12 +105,18 @@ class DNA_GAN(Module):
         self.generator = Generator(hidden_size, num_layers)
         self.discriminator = Discriminator(hidden_size, num_layers, seq_len)
 
-    def fit(self, lr, epochs, device):
+    def fit(self, train_data, lr, iters, device, save_to_wandb):
         criterion = CrossEntropyLoss()
         d_solver = Adam(self.discriminator.parameters(), lr=lr)
         g_solver = Adam(self.generator.parameters(), lr=lr)
 
-        for epoch in tqdm(range(epochs), desc='Training'):
-            
+        for iter in tqdm(range(iters), desc='Training'):
+            model, err_G, err_D = train(model, train_data, batch_size, seq_len, g_solver, d_solver, criterion, device)
+
+            meta = {'d_loss': err_D, 'g_loss': err_G}
+            if save_to_wandb:
+                wandb.log(meta)
+
+        return None
 
         
